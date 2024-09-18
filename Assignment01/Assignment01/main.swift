0b185d58d046eda5d1cf177f69e8e9a8b84f9f51
preet patel <preetpatelofficial004@gmail.com>
2024-09-17 20:06:39 -0400

    RoomResrvation Class

--- Assignment01/Assignment01/main.swift
+++ Assignment01/Assignment01/main.swift
@@ -9,12 +9,13 @@ import Foundation
 
 class Customer{
     let name : String
-    var email : String // when customer sign-up the email is assigned and can be changed
+    let email : String // when customer sign-up the email can't be changed
     var city : String // May change on long-time
     var creditCard : Int? // Customer can change the credit card later
     var description : CustomStringConvertible{
+        // if any detail of the customer is updated the description also gets updated
         get{
-            return "Name: \(name)\nEmail: \(email)\nCity: \(city)\nCredit Card: \(creditCard != nil ? "\(creditCard!)\n" : "No credit card provided.")\n"
+            return "Name: \(name)\nEmail: \(email)\nCity: \(city)\nCredit Card: \(creditCard != nil ? "\(creditCard!)\n" : "No credit card provided.")"
         }
     }
     //TODO: ASK PROF ABOUT ONE CONSTRUCTOR OVER MULTIPTIPLE
@@ -28,15 +29,59 @@ class Customer{
     
     
 }
-//var c1 = Customer(name: "Preet", city: "Delhi")
-//var c2 = Customer(name: "Maddy", email: "maddy@gmail.com", city: "Mumbai", creditCard: 1234)
-//
-//print(c1.description)
-//print(c2.description)
 
 class RoomReservation{
+    let customer : Customer // Reservation is only done by one customer not-changeable
+    let dailyRate : Double // reservation is booked on a fix daily rate
+    let numOfDays : Int // num if days for the reservation is also fixed
+    let taxRate : Double // Does not change for a reservation once fixed  }
+    var roomCost : Double{
+        get{
+            return dailyRate * Double(numOfDays)
+        }
+    }
+    var occupancyTax : Double{
+        get{
+            return roomCost * taxRate * 0.01
+        }
+    }
+    var total : Double {
+        get{
+            return roomCost + occupancyTax
+        }
+    }
+    
+    init(customer: Customer, dailyRate: Double, numOfDays: Int) {
+        self.customer = customer
+        self.dailyRate = dailyRate
+        self.numOfDays = numOfDays
+        self.taxRate = (self.customer.city == "New York City" ? 5.875: 2)
+    }
+    
+    func printInvoice(){
+        print("===================")
+        print("===== INVOICE =====")
+        print("===================")
+        print("---Customer Details---")
+        print(self.customer.description)
+        print("---Room Details---")
+        print("Daily Rate: $\(self.dailyRate)")
+        print("Length of stay: \(self.numOfDays) days")
+        print("Subtotal: $\(self.roomCost)")
+        print("Tax (\(self.taxRate)%): $\(self.occupancyTax)")
+        print("Total: $\(self.total)")
+    }
     
 }
+
 class ConferenceRoomReservation : RoomReservation{
     
 }
+
+
+var c1 = Customer(name: "Preet", city: "Delhi")
+var c2 = Customer(name: "Maddy", email: "maddy@gmail.com", city: "Mumbai", creditCard: 1234)
+
+var res1 = RoomReservation(customer: c1, dailyRate: 341.5, numOfDays: 3)
+
+res1.printInvoice()
