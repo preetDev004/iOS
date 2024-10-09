2d6b50f22032d86c080be7adf632a6548b82b31a
preet patel <preetpatelofficial004@gmail.com>
2024-10-08 23:16:30 -0400

    Week-6

--- PODCAST-PREET/PODCAST-PREET/ContentView.swift
+++ PODCAST-PREET/PODCAST-PREET/ContentView.swift
@@ -3,7 +3,6 @@
 //  PODCAST-PREET
 //
 //  Created by Preet Patel on 01/10/24.
-//
 
 import SwiftUI
 
@@ -60,40 +59,19 @@ struct ContentView: View {
             .padding(8)
             .frame(maxWidth: .infinity, alignment: .leading)
             
-//            Rectangle()
-//                .foregroundStyle(.white)
-//                .frame(height: 4)
-//                .padding(8)
-            
-            //! REMOVE LATER
-            ZStack(alignment: .leading) {
-                            // Background line
-                            Rectangle()
-                                .fill(Color(red:210/255,green: 210/255,blue: 210/255).opacity(0.3))
-                                .cornerRadius(50)
-                                .frame(height: 4)
-                        
-                            // Progress line
-                            Rectangle()
-                                .fill(Color.white)
-                                .cornerRadius(50)
-                                .frame(width: 89, height: 4)
-                            
-                            // Circle indicator
-                            Circle()
-                                .fill(Color.white)
-                                .frame(width: 16, height: 16)
-                                .shadow(radius: 2)
-                                .offset(x: 94 - 8)
-            }
-            .padding(.init(top: 8, leading: 8, bottom: 0, trailing: 8))
-            
+            Rectangle()
+                .foregroundStyle(.white)
+                .frame(height: 4)
+                .cornerRadius(5)
+                .padding(8)
+             
+           
             HStack{
-                Text("7:22")
+                Text("00:00")
                     .font(.system(size: 14))
                     .foregroundStyle(.white)
                 Spacer()
-                Text("-16:45")
+                Text("22:35")
                     .font(.system(size: 14))
                     .foregroundStyle(.white)
             }
@@ -132,7 +110,7 @@ struct ContentView: View {
                     Text("Theirs was a close-knit family, living in a small neighbourhood. And yet, when six suspicious deaths happened, no one raised even an eyebrow. Least of all, suspected their favourite daughter-in-law - Jolly.")
                         .font(.system(size: 14))
                         .foregroundStyle(Color(red:220/255, green: 220/255, blue: 220/255))
-                        .lineLimit(nil)
+
                 }
                 .padding(4)
             }
@@ -142,23 +120,11 @@ struct ContentView: View {
             .cornerRadius(8)
             .scrollIndicators(.hidden)
             
-            
-          
         }
         .padding()
-//        .background(Color(red:176/255, green:15/255, blue:18/255))
+        .background(Color(red:176/255, green:15/255, blue:18/255))
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(
-            LinearGradient(
-                gradient: Gradient(stops: [
-                    .init(color: Color(red: 176/255, green: 15/255, blue: 18/255), location: 0),
-                    .init(color: Color(red: 176/255, green: 15/255, blue: 18/255), location: 0.5),
-                    .init(color: Color(red: 83/255, green: 11/255, blue: 9/255), location: 1)
-                ]),
-                startPoint: .top,
-                endPoint: .bottom
-            )
-        )
+        
     }
 }
 
---
+++ w6/w6/w6App.swift
@@ -0,0 +1,17 @@
+//
+//  w6App.swift
+//  w6
+//
+//  Created by Preet Patel on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct w6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
