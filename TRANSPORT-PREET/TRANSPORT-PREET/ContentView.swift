72b6f3757e48d572e9b032f535c48c13c2e84ea6
preet patel <preetpatelofficial004@gmail.com>
2024-11-14 10:50:33 -0500

    Showing location on Map

--- TRANSPORT-PREET/TRANSPORT-PREET/ContentView.swift
+++ TRANSPORT-PREET/TRANSPORT-PREET/ContentView.swift
@@ -12,6 +12,7 @@ import MapKit
 struct Transpotation : Identifiable{
     let id : UUID = UUID()
     var name : String
+    var address : String
     var lat: Double
     var long: Double
     var category: MKPointOfInterestCategory
@@ -28,11 +29,13 @@ struct Transpotation : Identifiable{
         }
     }
     
-    init(name: String, lat: Double, long: Double, category: MKPointOfInterestCategory) {
+    init(name: String, address: String, lat: Double, long: Double, category: MKPointOfInterestCategory) {
         self.name = name
+        self.address = address
         self.lat = lat
         self.long = long
         self.category = category
+      
     }
 }
 
@@ -42,6 +45,7 @@ struct ContentView: View {
     @State var locationFromUI : String = ""
     @State var region : MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 0, longitude: 55), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     @State var searchResults : [Transpotation] = []
+    @State var selectedLocation: Transpotation? = nil
     
     func findPointsOfIntrest(location: String){
         let geocoder = CLGeocoder()
@@ -51,8 +55,6 @@ struct ContentView: View {
                 return
             }
             let coordinates : CLLocationCoordinate2D = placemarks![0].location!.coordinate
-
-//           region = MKCoordinateRegion(center: coordinates, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
             
             let request = MKLocalPointsOfInterestRequest(center: coordinates, radius: 1000)
             request.pointOfInterestFilter = MKPointOfInterestFilter(including: [.evCharger, .gasStation, .publicTransport])
@@ -65,9 +67,14 @@ struct ContentView: View {
                     print("No matching results!")
                     return
                 }
+                searchResults.removeAll()
                 for item in response!.mapItems{
+
                     let tsItem = Transpotation(
                         name: item.name!,
+                        address: (item.placemark.subThoroughfare != nil && item.placemark.thoroughfare != nil)
+                                    ?"\(item.placemark.subThoroughfare!) \(item.placemark.thoroughfare!)"
+                                    : "",
                         lat: item.placemark.coordinate.latitude,
                         long: item.placemark.coordinate.longitude,
                         category: item.pointOfInterestCategory!)
@@ -93,6 +100,7 @@ struct ContentView: View {
                 Button("Search"){
                     if(!locationFromUI.isEmpty){
                         findPointsOfIntrest(location: locationFromUI)
+                        locationFromUI = ""
                     }
                 }
                 .padding(12)
@@ -100,7 +108,9 @@ struct ContentView: View {
                 .foregroundStyle(.white)
                 .cornerRadius(4)
             }
-            Map(coordinateRegion: $region)
+            Map(coordinateRegion: $region, annotationItems: (selectedLocation != nil ? [selectedLocation!] : [])){ location in
+                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.lat, longitude: location.long))
+            }
                 .frame(width: 370, height: 300)
                 .cornerRadius(4)
             Spacer()
@@ -117,14 +127,17 @@ struct ContentView: View {
                            
                             VStack(alignment: .leading){
                                 Text(item.name)
-                                    .multilineTextAlignment(.leading)
+                                if(!item.address.isEmpty){
+                                    Text(item.address)
+                                }
                                 Text("\(item.lat), \(item.long)")
                                     .font(.system(size: 10))
                             }
                             Spacer()
                             Button("Map"){
-                                
-                            }
+                                region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: item.lat, longitude: item.long), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+                                selectedLocation = item
+                            }.buttonStyle(.borderless)
                         }
                     }
                     
